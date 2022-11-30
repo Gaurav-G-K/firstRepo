e1f2414dceb89222552fcd2788b787b9f32b9a7a
Gaurav-G-K <gauravkanaujiya588@gmail.com>
2022-11-30 11:31:14 +0530

    Prime No.

---
+++ primeNo.js
@@ -0,0 +1,16 @@
+let no=19;
+let isPrime=true;
+let i=2;
+while(i*i<=no)
+{
+if(no%i==0)
+{isPrime=false
+break;
+}
+else
+isPrime=true;
+}
+if(isPrime=true)
+console.log(no+" is Prime);
+else
+console.log(no+" is not Prime");
